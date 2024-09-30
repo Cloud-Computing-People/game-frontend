774587e3ae7b25c80120348dcae3d66099d9f479
Daren Hua <59425042+darenhua@users.noreply.github.com>
2024-09-30 19:57:40 -0400

    initialize react app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <div className="text-lg">
+                Hello CCP{" "}
+                <span className="text-sm text-muted-foreground">
+                    (cloud computing people)
+                </span>
+            </div>
+        </div>
+    );
+}
